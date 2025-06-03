cbd558a21c4584cbba1ad414c8888961c47fad9a
jay <wngml0708@gmail.com>
2025-06-03 12:19:33 +0900

    로그 필터 완성

--- src/main/java/co/kr/metacoding/backendtest/core/config/FilterConfig.java
+++ src/main/java/co/kr/metacoding/backendtest/core/config/FilterConfig.java
@@ -1,5 +1,6 @@
 package co.kr.metacoding.backendtest.core.config;
 
+import co.kr.metacoding.backendtest.core.filter.LogFilter;
 import co.kr.metacoding.backendtest.core.filter.UrlFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,15 @@ public class FilterConfig {
         FilterRegistrationBean<UrlFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new UrlFilter());
         registrationBean.addUrlPatterns("/*"); // 모든 요청에 적용
+        registrationBean.setOrder(2);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<LogFilter> logFilter() {
+        FilterRegistrationBean<LogFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LogFilter());
+        registrationBean.addUrlPatterns("/users/*"); // 모든 요청에 적용
         registrationBean.setOrder(1);
         return registrationBean;
     }
