84617285b2188943ed55ba34fd82b76454529ab5
JH <127887466+jh0804@users.noreply.github.com>
2025-06-03 11:27:02 +0900

    [feat] 유저 조회 (#3)
    /users/{id} API를 get 방식으로 호출하면, {"id": ?, "name": "?"}을 응답한다.

--- src/main/java/co/kr/metacoding/backendtest/user/UserController.java
+++ src/main/java/co/kr/metacoding/backendtest/user/UserController.java
@@ -1,11 +1,10 @@
 package co.kr.metacoding.backendtest.user;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,6 +14,12 @@ public class UserController {
     @PostMapping("/users")
     public ResponseEntity<?> join(@RequestBody UserRequest.JoinDTO reqDTO) {
         UserResponse.JoinDTO respDTO = userService.join(reqDTO);
+        return ResponseEntity.ok(respDTO);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getDetail(@PathVariable("id") Integer id) {
+        UserResponse.DTO respDTO = userService.getDetail(id);
         return ResponseEntity.ok(respDTO);
     }
 }
