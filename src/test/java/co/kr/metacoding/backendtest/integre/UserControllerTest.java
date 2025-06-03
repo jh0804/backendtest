51d3c2050998227f05f4846af4b86fcfc7cd6f32
JH <127887466+jh0804@users.noreply.github.com>
2025-06-03 14:15:19 +0900

    [fix] 유저 통합 테스트 (#9)
    url filter 상황 통합 테스트 추가

--- src/test/java/co/kr/metacoding/backendtest/integre/UserControllerTest.java
+++ src/test/java/co/kr/metacoding/backendtest/integre/UserControllerTest.java
@@ -2,6 +2,7 @@ package co.kr.metacoding.backendtest.integre;
 
 import co.kr.metacoding.backendtest.user.UserRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -123,9 +124,10 @@ public class UserControllerTest {
 
         // eye
         String responseBody = actions.andReturn().getResponse().getContentAsString();
-        System.out.println(responseBody);
+        // System.out.println(responseBody);
 
         // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.reason").value("해당 유저를 찾을 수 없습니다."));
 
     }
 
@@ -184,5 +186,28 @@ public class UserControllerTest {
 
         // then
         actions.andExpect(MockMvcResultMatchers.jsonPath("$.reason").value("해당 유저를 찾을 수 없습니다."));
+    }
+
+    // URL에 ? & = : //를 제외한 특수문자가 포함 시 차단 filter 테스트
+    @Test
+    public void get_detail_filter_test() throws Exception {
+        // given
+        Integer userId = 1;
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/users/{id}?name=test!!", userId)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.headers").isMap());
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.reason").value("허용되지 않은 특수문자가 URL에 포함되어 있습니다."));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.statusCode").value("BAD_REQUEST"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.statusCodeValue").value(400));
     }
 }
